b5567ecbc797f5ef32498d2e525af1266f3d0a86
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-04 13:44:50 +0530

    1. Installed Angular Material related packages. 2. Imported BrowserAnimationsModule in AppModule.

--- apps/customer-portal/src/app/app.module.ts
+++ apps/customer-portal/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { authRoutes, AuthModule } from '@demo-app/auth';
 
@@ -10,6 +11,8 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    
     RouterModule.forRoot([{ path: 'auth', children: authRoutes }], {
       initialNavigation: 'enabled',
     }),
