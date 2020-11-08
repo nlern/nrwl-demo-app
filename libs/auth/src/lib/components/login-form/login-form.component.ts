dbe28e9975b70154bf00046e18d2251eceb44b8d
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-08 20:19:51 +0530

    Renamed css files to scss.

--- libs/auth/src/lib/components/login-form/login-form.component.ts
+++ libs/auth/src/lib/components/login-form/login-form.component.ts
@@ -6,7 +6,7 @@ import { Authenticate } from '@demo-app/data-models';
 @Component({
   selector: 'demo-app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css'],
+  styleUrls: ['./login-form.component.scss'],
 })
 export class LoginFormComponent {
   @Output() submit = new EventEmitter<Authenticate>();
--- libs/auth/src/lib/containers/login/login.component.ts
+++ libs/auth/src/lib/containers/login/login.component.ts
@@ -4,7 +4,7 @@ import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'demo-app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent implements OnInit {
