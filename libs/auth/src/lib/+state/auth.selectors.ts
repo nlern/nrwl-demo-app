5e8f4d6042bed8f13337775c34972a024e2e89b5
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-09 12:02:04 +0530

    Updated auth selectors by removing entity references.

--- libs/auth/src/lib/+state/auth.selectors.ts
+++ libs/auth/src/lib/+state/auth.selectors.ts
@@ -1,17 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import {
-  AUTH_FEATURE_KEY,
-  State,
-  AuthPartialState,
-  authAdapter,
-} from './auth.reducer';
+import { AUTH_FEATURE_KEY, State, AuthPartialState } from './auth.reducer';
 
 // Lookup the 'Auth' feature state managed by NgRx
 export const getAuthState = createFeatureSelector<AuthPartialState, State>(
   AUTH_FEATURE_KEY
 );
-
-const { selectAll, selectEntities } = authAdapter.getSelectors();
 
 export const getAuthLoaded = createSelector(
   getAuthState,
@@ -23,21 +16,7 @@ export const getAuthError = createSelector(
   (state: State) => state.error
 );
 
-export const getAllAuth = createSelector(getAuthState, (state: State) =>
-  selectAll(state)
-);
-
-export const getAuthEntities = createSelector(getAuthState, (state: State) =>
-  selectEntities(state)
-);
-
-export const getSelectedId = createSelector(
+export const getAuthUser = createSelector(
   getAuthState,
-  (state: State) => state.selectedId
-);
-
-export const getSelected = createSelector(
-  getAuthEntities,
-  getSelectedId,
-  (entities, selectedId) => selectedId && entities[selectedId]
+  (state: State) => state.user
 );
