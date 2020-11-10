73169325c3e5a2a4fcc1161ce578edcaa1c0e35e
Shantanu Dutta <shantanudutta1994@gmail.com>
2020-11-10 14:47:49 +0530

    1. Created products list component.

--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -9,6 +9,7 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromProducts from './+state/products.reducer';
 import { ProductsEffects } from './+state/products.effects';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   imports: [
@@ -21,6 +22,6 @@ import { ProductsEffects } from './+state/products.effects';
     ),
     EffectsModule.forFeature([ProductsEffects]),
   ],
-  declarations: [ProductsComponent],
+  declarations: [ProductsComponent, ProductListComponent],
 })
 export class ProductsModule {}
